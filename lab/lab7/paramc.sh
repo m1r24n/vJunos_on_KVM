dee142fa8279e7e046299b5eade82bca25873330
m1r24n <m.irzan@gmail.com>
2026-03-25 14:33:05 +0000

    update

---
+++ lab/lab7/paramc.sh
@@ -0,0 +1,7 @@
+export LXC_NAME=cl1sw4
+export VLAN=41
+export OVS=sw4_cl5
+export IPv4=10.1.41.11/24
+export GWv4=10.1.41.1
+export IPv6=fc00:dead:beef:a041::1000:11/64
+export GWv6=fc00:dead:beef:a041::1
